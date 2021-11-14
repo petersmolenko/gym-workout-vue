6a2a87c972454322c65a9faada6df5633df62be0
Peter Smolenko <smolenko.p@yandex.ru>
2021-11-14 22:49:42 +0300

    Created utils

---
+++ src/utils/data.js
@@ -0,0 +1,10 @@
+export const getMuscleGroupCaptionByAlias = (alias) => {
+    switch(alias.toLowerCase()) {
+        case "legs": return 'Ноги';
+        case "hands": return 'Руки';
+        case "shoulders": return 'Плечи';
+        case "back": return 'Спина';
+        case "chest": return 'Грудь';
+        default: return 'Неопределенная группа мышц';
+    }
+}
